143a09be56405731b1ee8a485984055fe1834ec9
Samuel-3004 <samuelszportela@hotmail.com>
2023-12-22 14:29:29 -0300

    I created the methods for listing, registering and removing a vehicle

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -22,5 +22,29 @@ namespace trilha_dotnet_fundamentos_desafio.Models
         public decimal PrecoInicial { get; set; }
         public decimal PrecoPorHora { get; set; }
         public List<VeiculoEstacionado> Veiculos { get; set; }
+
+        public void CadastrarVeiculo(VeiculoEstacionado newVeiculo)
+        {
+            Console.WriteLine("Pressione qualquer tecla para continuar.");
+            Console.ReadKey();
+            Veiculos.Add(newVeiculo);
+        }
+
+        public void ListarVeiculos()
+        {
+            Console.WriteLine("Os carros estacionados são:");
+            foreach (VeiculoEstacionado veiculo in Veiculos)
+            {
+                Console.WriteLine(veiculo.PlacaDoVeiculo);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar.");
+            Console.ReadKey();
+        }
+
+        public bool RemoverVeiculo(VeiculoEstacionado sairVeiculo)
+        {
+            bool success = Veiculos.Remove(sairVeiculo);
+            return success;
+        }
     }
 }

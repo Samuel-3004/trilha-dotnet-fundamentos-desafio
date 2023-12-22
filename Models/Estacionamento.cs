c2a69f723ec5859e276bae368a29646d9623b786
Samuel-3004 <samuelszportela@hotmail.com>
2023-12-22 14:46:27 -0300

    fix: change in namespace

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace trilha_dotnet_fundamentos_desafio.Models
+namespace TrilhaDotnetFundamentosDesafio.Models
 {
     public class Estacionamento
     {
